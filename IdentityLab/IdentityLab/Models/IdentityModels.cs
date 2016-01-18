11d755cf946b144279b15bd7850a3ae74a99c978
JuanGRomeo <juan.romeo.monreal@gmail.com>
2016-01-18 20:21:58 +0100

    Explore Identity 2.0. Users and Roles customizated. Extend Identity Models to use an int as PK.

---
+++ IdentityLab/IdentityLab/Models/IdentityModels.cs
@@ -0,0 +1,173 @@
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.Data.Entity;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+
+namespace IdentitySample.Models
+{
+    // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit http://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
+    //public class ApplicationUser : IdentityUser
+    //{
+    //    public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager)
+    //    {
+    //        // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
+    //        var userIdentity = await manager.CreateIdentityAsync(this, DefaultAuthenticationTypes.ApplicationCookie);
+    //        // Add custom user claims here
+    //        return userIdentity;
+    //    }
+
+    //    public string Address { get; set; }
+    //    public string City { get; set; }
+    //    public string State { get; set; }
+
+    //    // Use a sensible display name for views:
+    //    [Display(Name = "Postal Code")]
+    //    public string PostalCode { get; set; }
+
+    //    // Concatenate the address info for display in tables and such:
+    //    public string DisplayAddress
+    //    {
+    //        get
+    //        {
+    //            string dspAddress =
+    //                string.IsNullOrWhiteSpace(this.Address) ? "" : this.Address;
+    //            string dspCity =
+    //                string.IsNullOrWhiteSpace(this.City) ? "" : this.City;
+    //            string dspState =
+    //                string.IsNullOrWhiteSpace(this.State) ? "" : this.State;
+    //            string dspPostalCode =
+    //                string.IsNullOrWhiteSpace(this.PostalCode) ? "" : this.PostalCode;
+
+    //            return string
+    //                .Format("{0} {1} {2} {3}", dspAddress, dspCity, dspState, dspPostalCode);
+    //        }
+    //    }
+    //}
+
+    //CUSTOM APPLICATION USER
+    public class ApplicationUser: IdentityUser<int, ApplicationUserLogin, ApplicationUserRole, ApplicationUserClaim>, IUser<int>
+    {
+        public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser, int> manager)
+        {
+            // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
+            var userIdentity = await manager.CreateIdentityAsync(this, DefaultAuthenticationTypes.ApplicationCookie);
+            // Add custom user claims here
+            return userIdentity;
+        }
+
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+
+        // Use a sensible display name for views:
+        [Display(Name = "Postal Code")]
+        public string PostalCode { get; set; }
+
+        // Concatenate the address info for display in tables and such:
+        public string DisplayAddress
+        {
+            get
+            {
+                string dspAddress =
+                    string.IsNullOrWhiteSpace(this.Address) ? "" : this.Address;
+                string dspCity =
+                    string.IsNullOrWhiteSpace(this.City) ? "" : this.City;
+                string dspState =
+                    string.IsNullOrWhiteSpace(this.State) ? "" : this.State;
+                string dspPostalCode =
+                    string.IsNullOrWhiteSpace(this.PostalCode) ? "" : this.PostalCode;
+
+                return string
+                    .Format("{0} {1} {2} {3}", dspAddress, dspCity, dspState, dspPostalCode);
+            }
+        }
+    }
+
+    //CUSTOM APPLICATION DB CONTEXT
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser, ApplicationRole, int,
+        ApplicationUserLogin, ApplicationUserRole, ApplicationUserClaim>
+    {
+        public ApplicationDbContext()
+            : base("DefaultConnection")
+        {
+        }
+
+        static ApplicationDbContext()
+        {
+            // Set the database intializer which is run once during application start
+            // This seeds the database with admin user credentials and admin role
+            Database.SetInitializer<ApplicationDbContext>(new ApplicationDbInitializer());
+        }
+
+        public static ApplicationDbContext Create()
+        {
+            return new ApplicationDbContext();
+        }
+    }
+
+    #region Integer-Based Definitions
+
+    public class ApplicationUserLogin : IdentityUserLogin<int> { }
+    public class ApplicationUserClaim : IdentityUserClaim<int> { }
+    public class ApplicationUserRole : IdentityUserRole<int> { }
+
+    #endregion
+
+    public class ApplicationRole : IdentityRole<int, ApplicationUserRole>, IRole<int>
+    {
+        public string Description { get; set; }
+
+        public ApplicationRole() { }
+        public ApplicationRole(string name)
+            : this()
+        {
+            this.Name = name;
+        }
+
+        public ApplicationRole(string name, string description)
+            : this(name)
+        {
+            this.Description = description;
+        }
+    }
+
+    //CUSTOM USER STORE
+    public class ApplicationUserStore :
+        UserStore<ApplicationUser, ApplicationRole, int,
+        ApplicationUserLogin, ApplicationUserRole,
+        ApplicationUserClaim>, IUserStore<ApplicationUser, int>,
+        IDisposable
+    {
+        public ApplicationUserStore() : this(new IdentityDbContext())
+        {
+            base.DisposeContext = true;
+        }
+
+        public ApplicationUserStore(DbContext context)
+            : base(context)
+        {
+        }
+    }
+
+    //CUSTOM ROLE STORE
+    public class ApplicationRoleStore
+        : RoleStore<ApplicationRole, int, ApplicationUserRole>,
+        IQueryableRoleStore<ApplicationRole, int>,
+        IRoleStore<ApplicationRole, int>, IDisposable
+    {
+        public ApplicationRoleStore()
+            : base(new IdentityDbContext())
+        {
+            base.DisposeContext = true;
+        }
+
+        public ApplicationRoleStore(DbContext context)
+            : base(context)
+        {
+        }
+    }
+}
